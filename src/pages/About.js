884ca23bb8c956d02bb03672e8232276daa17c94
codemasterimran <96138445+codemasterimran@users.noreply.github.com>
2024-11-10 18:02:59 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,9 @@ function About() {
   return (
     <div className="about-content">
       <h2>About MusicGen</h2>
-      <p>MusicGen is a platform that allows users to generate music based on their input prompts. Simply enter your ideas, and let MusicGen create unique musical experiences for you!</p>
+      <p> Greetings from Sangeet AI, where innovation and creativity coexist! Whether you're an experienced musician or a fervent fan with no prior expertise, we think that everyone should be able to create music. With only a few suggestions, our software may help you unleash your creative potential and create original, customised music. Sangeet AI effortlessly transforms your thoughts into musical masterpieces because to its sophisticated AI-driven algorithms and user-friendly interface.
+
+The limitless potential of music and technology inspires us at Sangeet AI. With only a few lines, we hope to enable musicians, artists, and dreamers to realise their musical fantasies. So feel free to share your thoughts with us, and we will turn them into sound. Whether you want to write lively songs, mellow melodies, or cinematic soundscapes, we’re here to make it happen. </p>
     </div>
   );
 }
